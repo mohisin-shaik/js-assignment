7589123cdcc6c9c717e9e0c56ceb9941173e08be
mohisin-shaik <mohisin3.in@gmail.com>
2020-08-13 21:31:31 +0530

    folderupload

--- Day4/scripts/q3.js
+++
@@ -1,9 +0,0 @@
-let  shoppinglist = ["Eggs","Butter","Maida","Brush","Oil","Dal","Thordal","Honey"];
-
-let shoppingBasket = shoppinglist;
-shoppingBasket.push("Onions");
-shoppingBasket.push("Sugar");
-shoppingBasket.push("shampoo");
-shoppingBasket.push("Chilly");
-shoppingBasket.push("Milk packet")
-console.log(shoppingBasket);
